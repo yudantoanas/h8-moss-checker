ea6e3b1dc8f3ecf31e6a8c361980a9b3083c1b2c
Teddy K <teddykoerniadi@gmail.com>
2023-09-20 11:09:21 +0700

    update config and username example

--- config/config.example.js
+++ config/config.example.js
@@ -1,4 +1,5 @@
 module.exports = {
-  batch_name: "batch-name",
+  batch_name: "H8-P2-S3", // example
+  prefix_name: "p2-c1-server",
   base_ratio: 0.8
 }
---
+++ config/username.example.js
@@ -0,0 +1,5 @@
+module.exports = [
+  "username1",
+  "username2",
+  "username3",
+]
